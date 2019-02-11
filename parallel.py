60f5cba9a0c9ced8e462fdf540f7cbb7463eebfe
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-02-11 09:09:21 -0500

    masking transits

--- parallel.py
+++ parallel.py
@@ -16,15 +16,15 @@ from multiprocessing import Pool
 sys.path.append(os.getcwd())
 
 def measure_prot(row):
-    star = row[1]
-    starname = str(int(star.kepid)).zfill(9)
+    df = row[1]
+    starname = str(int(df.kepid)).zfill(9)
 
     lcpath = "data/lightcurves/{}".format(starname)
-    time, flux, flux_err = rt.download_light_curves(star.kepid, ".", lcpath)
+    time, flux, flux_err = rt.download_light_curves(df.kepid, ".", lcpath)
 
     # Measure rotation period
-    rotate = sr.RotationModel(time, flux, flux_err, star.kepid)
-    t0, dur, porb = None, None, None
+    rotate = sr.RotationModel(time, flux, flux_err, df.kepid, plot=True)
+    t0, dur, porb = df.koi_time0, df.koi_duration, df.koi_period
     gp_stuff, ls_period, acf_period = rotate.measure_rotation_period(
         t0, dur, porb)
     gp_period, errp, errm, Q, Qerrp, Qerrm = gp_stuff
@@ -33,16 +33,16 @@ def measure_prot(row):
           "acf period = ", acf_period)
 
     # Save results
-    star["gp_period"] = gp_period
-    star["gp_period_errp"] = errp
-    star["gp_period_errm"] = errm
-    star["logQ"] = Q
-    star["logQ_errp"] = Qerrp
-    star["logQ_errm"] = Qerrm
-    star["acf_period"] = acf_period
-    star["ls_period"] = ls_period
+    df["gp_period"] = gp_period
+    df["gp_period_errp"] = errp
+    df["gp_period_errm"] = errm
+    df["logQ"] = Q
+    df["logQ_errp"] = Qerrp
+    df["logQ_errm"] = Qerrm
+    df["acf_period"] = acf_period
+    df["ls_period"] = ls_period
 
-    star.to_csv("{}_results.csv".format(str(int(star.kepid)).zfill(9)))
+    df.to_csv("{}_results.csv".format(str(int(star.kepid)).zfill(9)))
 
 
 if __name__ == "__main__":
